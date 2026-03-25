c1a897bb554ed168512e78860313f157818b7a27
Blohin123 <triblohi.2008@gmail.com>
2026-03-25 15:45:21 +0300

    !!!!

--- hi.cpp
+++ hi.cpp
@@ -2,7 +2,7 @@
 
 int main(){
     std::cout << "First line" << std::endl;
-    std::cout << "Hello world" << std::endl;
+    std::cout << "Hello world!!!" << std::endl;
     std::cout << "By by Russa!" << std::endl;
     std::cout << "Last line" << std::endl;
     return 0;
