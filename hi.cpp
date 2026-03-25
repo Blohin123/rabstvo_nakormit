2ecde2709d8f9b89b202b06c52c00ad69554fe77
Blohin <triblohi.2008@gmail.com>
2026-03-25 12:43:40 +0000

    feat: add ! * mnogo

--- hi.cpp
+++ hi.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 int main(){
-    std::cout << "Hello world" << std::endl;
+    std::cout << "Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
     std::cout << "By by Russa!" << std::endl;
     std::cout << "Last line" << std::endl;
     return 0;
