f4b51adcb14729acb3a0d9efa17a10d60a102be0
Blohin <triblohi.2008@gmail.com>
2026-09-25 15:39:24 +0300

    Delete hi.cpp

--- hi.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-int main(){
-    std::cout << "First line" << std::endl;
-    std::cout << "Hello world!!!" << std::endl;
-    std::cout << "By by Russa!" << std::endl;
-    std::cout << "Last line" << std::endl;
-    return 0;
-}
